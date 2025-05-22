2d1e006331a8418dcf5f20ea6a74c5332413fb7e
akira <eduardoakiramurata@gmail.com>
2025-05-22 20:38:43 -0300

    Fix: remove route filterChain

--- src/main/java/br/com/fiap/auth/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/auth/config/SecurityConfig.java
@@ -35,8 +35,7 @@ public class SecurityConfig {
                     "/v3/api-docs/**",
                     "/swagger-ui/**",
                     "/swagger-ui.html",
-                    "/h2-console/**",
-                    "/suitability"
+                    "/h2-console/**"
                 ).permitAll()
                 .anyRequest().authenticated()
             )
