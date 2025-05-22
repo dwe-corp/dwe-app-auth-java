44a1acb39a7d3f9c4fcf05d36316e0d02a862889
akira <eduardoakiramurata@gmail.com>
2025-05-21 21:50:55 -0300

    Adjusts: Port & Secret Key

--- src/main/java/br/com/fiap/auth/util/JwtUtil.java
+++ src/main/java/br/com/fiap/auth/util/JwtUtil.java
@@ -2,11 +2,11 @@ package br.com.fiap.auth.util;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
@@ -16,7 +16,7 @@ import java.util.function.Function;
 @Component
 public class JwtUtil {
 
-    private final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final Key SECRET_KEY = Keys.hmacShaKeyFor("dwe-one-super-secreta-chave-jwt-256-bits".getBytes(StandardCharsets.UTF_8));
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
